f4415f44fefb7d1e116917f64cb50a5781489ef2
Xin Wang <xin@xinwang.me>
2022-02-05 12:22:50 -0800

    added theme, login-page, configured chakra ui

---
+++ client/src/login-page.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Flex,
+  Grid,
+  theme,
+} from '@chakra-ui/react';
+
+
+const LoginPage = () => {
+  <VStack>
+  <Box fontSize="xl">
+    <Text>
+      Spotability
+    </Text>
+  </Box>
+  </VStack>
+}
+
+export default LoginPage
---
+++ client/src/theme.js
@@ -0,0 +1,36 @@
+import { extendTheme, Stack } from '@chakra-ui/react'
+import { mode } from '@chakra-ui/theme-tools';
+import { createBreakpoints } from '@chakra-ui/theme-tools'
+
+
+// const additional_colors = {
+//     colors: {
+//     bg_pink: {
+//       100: "fae1dd",
+//       200: "fcd5ce",
+//       300: "fec5bb",
+//     }
+// }
+
+const config = {
+  initialColorMode: 'light',
+  useSystemColorMode: false,
+}
+
+const styles = {
+  global: (props) => ({
+    body: {
+      bg: mode('#fec5bb', '#121212')(props),
+      color: mode('#ffffff', 'fec5bb')(props),
+      fontSize: '20px',
+    }
+  })
+}
+
+const theme = extendTheme({
+  config,
+  styles,
+})
+
+
+export default theme;
