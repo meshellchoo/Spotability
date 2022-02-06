9cda234d14af550c8ffd4acf100972162737bbfd
Yuteng Terrence Wu <yutengwu@yahoo.com>
2022-02-05 23:01:00 -0800

    home page

--- client/src/pages/home-page/home-page.js
+++ client/src/pages/home-page/home-page.js
@@ -5,11 +5,11 @@ import {
   Flex,  useColorMode,
   Image,
   IconButton,
-  Button,
   Heading,
   Wrap,
   WrapItem,
   Center,
+  Button,
 } from '@chakra-ui/react';
 import {
   SunIcon,
@@ -20,65 +20,160 @@ import {
 function HomePage () {
   const {colorMode, toggleColorMode} = useColorMode();
   return (
-  <VStack>
+//   <VStack>
+    // <Box my={10}>
+    //   <IconButton size="lg" colorScheme={colorMode ==='dark'? "pink" : "blackAlpha"} variant="solid" shadow="lg" borderRadius="full" onClick={toggleColorMode} icon={colorMode === 'dark' ? <SunIcon/> : <MoonIcon/>}>
+        
+    //   </IconButton>
+    // </Box>
+
+//     <Box>
+//         <Flex my="20">
+//             <Button colorScheme='fec5bb' borderColor='fec5bb'  _hover={{ bg: '#fec5bb' }}>
+//                 <Image
+//                     src="avatar.svg"
+//                     boxSize='200px'
+//                 >
+//                 </Image>
+//             </Button>
+//         </Flex>
+//     </Box>    
+
+    
+//     <Box>
+//         <Flex my="10">
+//             <Heading fontSize="5xl" fontWeight="700" fontStyle="bold">
+//                 Trending Now
+//             </Heading>
+//         </Flex>
+//     </Box>
+
+//     <Flex>
+//     <Text>
+//         test
+//     </Text>
+//     </Flex>
+
+
+
+    
+//     <Wrap>
+//   <WrapItem>
+//     <Center w='200px' h='80px' bg='red.200'>
+      
+//     </Center>
+//   </WrapItem>
+//   <WrapItem>
+//     <Center w='200px' h='80px' bg='red.200'>
+//       Box 2
+//     </Center>
+//   </WrapItem>
+//   <WrapItem>
+//     <Center w='200px' h='80px' bg='red.200'>
+//       Box 3
+//     </Center>
+//   </WrapItem>
+//   <WrapItem>
+//     <Center w='200px' h='80px' bg='red.200'>
+//       Box 4
+//     </Center>
+//   </WrapItem>
+// </Wrap>
+    
+    
+
+    
+
+    
+    
+//   </VStack>
+
+
+
+<VStack >
     <Box my={10}>
       <IconButton size="lg" colorScheme={colorMode ==='dark'? "pink" : "blackAlpha"} variant="solid" shadow="lg" borderRadius="full" onClick={toggleColorMode} icon={colorMode === 'dark' ? <SunIcon/> : <MoonIcon/>}>
         
       </IconButton>
     </Box>
 
-    <Box>
-        <Flex my="20">
-            <Button colorScheme='fec5bb' borderColor='fec5bb'  _hover={{ bg: '#fec5bb' }}>
-                <Image
-                    src="avatar.svg"
-                    boxSize='200px'
-                >
-                </Image>
-            </Button>
-        </Flex>
-    </Box>    
+    <Flex direction="column">
+        <Box>
+          <Image src="landing-page-image.svg"></Image>
+        </Box>
 
-    
-    <Box>
-        <Flex my="10">
-            <Heading fontSize="5xl" fontWeight="700" fontStyle="bold">
-                Trending Now
+
+        <Box>
+            <Heading fontSize="6xl" fontWeight="700" fontStyle="bold">
+              Trending Now
             </Heading>
-        </Flex>
-    </Box>
-    
-    
-    <Wrap>
-  <WrapItem>
-    <Center w='180px' h='80px' bg='red.200'>
-      Box 1
-    </Center>
-  </WrapItem>
-  <WrapItem>
-    <Center w='180px' h='80px' bg='red.200'>
-      Box 2
-    </Center>
-  </WrapItem>
-  <WrapItem>
-    <Center w='180px' h='80px' bg='red.200'>
-      Box 3
-    </Center>
-  </WrapItem>
-  <WrapItem>
-    <Center w='180px' h='80px' bg='red.200'>
-      Box 4
-    </Center>
-  </WrapItem>
-</Wrap>
-    
-    
+        </Box>
+        
+        <Wrap>
+            <WrapItem>
+                <Center w='200px' h='300px' bg='red.200'>
+      
+                    </Center>
+            </WrapItem>
+            <WrapItem>
+                <Center w='200px' h='300px' bg='red.200'>
+                    
+                    </Center>
+                </WrapItem>
+            <WrapItem>
+                <Center w='200px' h='300px' bg='red.200'>
+                    
+                    </Center>
+            </WrapItem>
+            <WrapItem>
+                <Center w='200px' h='300px' bg='red.200'>
+                    
+                    </Center>
+            </WrapItem>
+        </Wrap>
+      
+        <Flex my="40px"/>
 
-    
+        <Box>
+            <Center>
+                <Button size="lg" 
+                bgGradient='linear(to-r, green.200, pink.500)' 
+                _hover={{ bg: '#fb76dc' }} >
+                    Match Now
+                </Button>
+            </Center>
+        </Box>
+        
+        <Flex my="40px"/>
 
-    
-    
-  </VStack>
+
+        <Wrap>
+            <WrapItem>
+                <Center w='200px' h='300px' bg='red.200'>
+      
+                    </Center>
+            </WrapItem>
+            <WrapItem>
+                <Center w='200px' h='300px' bg='red.200'>
+                    
+                    </Center>
+                </WrapItem>
+            <WrapItem>
+                <Center w='200px' h='300px' bg='red.200'>
+                    
+                    </Center>
+            </WrapItem>
+            <WrapItem>
+                <Center w='200px' h='300px' bg='red.200'>
+                    
+                    </Center>
+            </WrapItem>
+        </Wrap>
+        
+
+
+    </Flex>
+</VStack>
   )
 }
 
