749cdbc5af52350ad3e7b2df96919b65dbed9c25
Michelle Chu <michellechu0131@gmail.com>
2022-02-05 16:22:37 -0800

    Able to redirect to Spotify Auth

--- server/Spotability/urls.py
+++ server/Spotability/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import get
+from .views import get, spotify_callback
 from .database import test 
 
 appname = 'Spotability'
 urlpatterns = [
     path('spotify-url/', get),
-    # path('redirect/', AuthURL.spotify_callback),
+    path('redirect/', spotify_callback),
     # path('is-authenticated/', isAuthenticated.as_view()),
     path('test/', test),
 ]
--- server/Spotability/views.py
+++ server/Spotability/views.py
@@ -23,6 +23,7 @@ from .util import get_user_tokens, is_spotify_authenticated, update_or_create_us
 def get(request):
     # check for scopes here -> https://developer.spotify.com/documentation/general/guides/authorization/scopes/
     scopes = 'user-read-private user-read-email user-top-read'
+    print('test',REDIRECT_URI)
     url = Request('GET','https://accounts.spotify.com/authorize',params={
         'scope': scopes,
         'response_type': 'code',
@@ -31,31 +32,30 @@ def get(request):
         
     }).prepare().url
     return JsonResponse({'url':url}, status=status.HTTP_200_OK)
-    # def spotify_callback(request, format=None):
-    #     code = request.GET.get('code')
-    #     error = request.GET.get('error')
-        
-    #     response = post('https://accounts.spotify.com/api/token',data={
-    #         'grant_type' : 'authorization_code',
-    #         'code': 'code',
-    #         'redirect_uri': REDIRECT_URI,
-    #         'client_id': CLIENT_ID,
-    #         'client_secret': CLIENT_SECRET,
-    #     }).json()
-        
-    #     access_token = response.get('access_token')
-    #     token_type = response.get('token_type')
-    #     refresh_token = response.get('refresh_token')
-    #     expires_in = response.get('expires_in')
-    #     error = response.get('error')
-        
-    #     # if not request.session.exists(request.session.session_key):
-    #     #     request.session.create()
-            
-            
-    #     # update_or_create_user_tokens(
-    #     #     request.session.session_key, access_token, token_type, expires_in, refresh_token)
-    #     return redirect('Spotability:')
+
+
+def spotify_callback(request, format=None):
+    code = request.GET.get('code')
+    error = request.GET.get('error')
+    
+    response = post('https://accounts.spotify.com/api/token',data={
+        'grant_type' : 'authorization_code',
+        'code': 'code',
+        'redirect_uri': REDIRECT_URI,
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+    }).json()
+    
+    access_token = response.get('access_token')
+    token_type = response.get('token_type')
+    refresh_token = response.get('refresh_token')
+    expires_in = response.get('expires_in')
+    error = response.get('error')
+    
+    if not response.ok:
+        return Response({'error': 'Spotify request failed!'}, status=response.status_code)
+    return Response(response.json(), status=status.HTTP_200_OK)
+
 
 
 # class isAuthenticated(APIView):
