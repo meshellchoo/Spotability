9c4125f0981816ef3c6da8cf774a4626744ab41e
Jeng-Rung Tu <46553063+JengRung@users.noreply.github.com>
2022-02-06 06:05:49 -0800

    Fix front end

--- client/src/index.js
+++ client/src/index.js
@@ -24,7 +24,7 @@ ReactDOM.render(
         <Route path="/" element={<LandingPage />} />
 
         {/* <Route path="/user/:accessToken/:refreshToken" component={<HomePage/>} /> */}
-        {/* <Route path="/spotability/redirect/:email" element={<App />} /> */}
+        <Route path="/spotability/redirect/:email" element={<App />} />
         <Route path="/spotability/redirect/" element={<App />} />
         {/* :email means it is a param */}
       </Routes>  
