6cb92b68d60f96efaad71c70fbfd7d33deb04518
Michelle Chu <michellechu0131@gmail.com>
2022-02-06 03:05:45 -0800

    Added analytics for recommended tracks

--- server/Spotability/analytics.py
+++ server/Spotability/analytics.py
@@ -15,10 +15,25 @@ def get_top_genre(request):
     print(email)
     obj = SpotabilityCollection()
     user = obj.search_by_email(email)
-    return JsonResponse(user['top_genres'][0])
+    return JsonResponse({"top_genre":user['top_genres'][0]})
 
 def get_top_track_from_top_genre(request):
     pass
 
+# ex: https://api.spotify.com/v1/recommendations?seed_genres=classical,country 
 def get_recommended_track(request):
-    pass
+    """
+    query params: 'email' 
+    """
+    email = request.GET.get('email')
+    obj = SpotabilityCollection()
+    user = obj.search_by_email(email)
+    least_five_genres= user["top_genres"][-6:-1]
+    print(least_five_genres)
+    URL = '	https://api.spotify.com/v1/recommendations'
+    token = "Bearer " + user['access_token']
+    headers={'Authorization': token}
+    params={'seed_genres':least_five_genres}
+    response = requests.get(URL,headers=headers,params=params)
+    data = response.json()
+    return data
